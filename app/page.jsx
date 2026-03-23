ce1a7932caa6544a19d738d7c6cbd078c4539b24
Uwe Erichsen <u.erichsen78@gmail.com>
2026-03-23 12:38:02 +0100

    fix useEffect

--- app/page.jsx
+++ app/page.jsx
@@ -267,6 +267,7 @@ const [password, setPassword] = useState("");
         setIsLoggedIn(!!user.name);
       }
     } catch {}
+},[]);
 
     const hash = window.location.hash.replace("#shared=", "");
     if (hash) {
